a8b4a157379283722ea96d8bffdaa3a7a84a19dc
Cindy-lmz <73512807+Cindy-lmz@users.noreply.github.com>
2021-05-14 18:25:53 +0800

    Update common.php

--- src/common.php
+++ src/common.php
@@ -270,10 +270,6 @@ if (class_exists('think\Console')) {
     ]);
 }
 
-// 加载对应的语言包
-Lang::load(__DIR__ . '/lang/zh-cn.php', 'zh-cn');
-Lang::load(__DIR__ . '/lang/en-us.php', 'en-us');
-
 // 动态加载模块配置
 if (function_exists('think\__include_file')) {
     $root = rtrim(str_replace('\\', '/', env('app_path')), '/');
